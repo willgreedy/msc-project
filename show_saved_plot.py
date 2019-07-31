b51120b9f98dc15ba5a7f9370c60aad6d47a6bed
William Greedy <willgreedy@gmail.com>
2019-07-31 18:08:19 +0100

    Added basic analysis to plot showing. Added higher noise config.

--- show_saved_plot.py
+++ show_saved_plot.py
@@ -1,7 +1,9 @@
 import pickle
+import matplotlib
 from matplotlib import pyplot as plt
 import os
 import sys
+import numpy as np
 
 
 def show_saved_plots(plot_location):
@@ -9,6 +11,18 @@ def show_saved_plots(plot_location):
     for filename in os.listdir(plot_location):
         with open(plot_location + filename, 'rb') as file:
             fig = pickle.load(file)
+            fig.canvas.set_window_title(filename)
+
+            ax = fig.axes[0]
+            line = ax.lines[0]
+            xs = np.array(line.get_xdata())
+            ys = np.array(line.get_ydata())
+
+            min_x = 500 * 500000 + 1000000 / 2
+            max_x = min_x + 1000000
+            mean_test_value = np.mean(ys[np.bitwise_and(xs > min_x, (xs <= max_x))])
+            print("{}: {}".format(filename, mean_test_value))
+
     plt.show()
 
 
@@ -19,6 +33,7 @@ def show_saved_plot(plot_location):
     plt.show()
 
 if __name__ == '__main__':
+    matplotlib.rcParams['agg.path.chunksize'] = 10000
     plot_location = sys.argv[1]
     if plot_location[-4:] == '.pkl':
         show_saved_plot(plot_location)
